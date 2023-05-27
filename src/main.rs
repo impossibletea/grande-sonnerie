4006b759bd1e4faca423084e9cc5400c287cbd65
Zakhar Tarshis <impossibletea@icloud.com>
2023-05-27 12:01:04 +0300

    Add basic logic

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,57 @@
 use std::{
-    fs::File,
-    io::BufReader,
+    thread,
+    time::{SystemTime, Duration},
 };
 
 use rusty_audio::Audio;
-use serde::{Serialize, Deserialize};
-use time::{UtcOffset, OffsetDateTime};
-
-mod sonnerie;
-use sonnerie::Sonnerie;
-
-#[derive(Serialize, Deserialize)]
-struct Config {
-}
 
 fn main() {
-    let file = "test.wav";
-    let offset = UtcOffset::from_hms(7, 0, 0).unwrap_or(UtcOffset::UTC);
+    let files = [
+        "grande1",
+        "grande2",
+        "hour",
+        "minute"
+    ];
     let mut audio = Audio::new();
-    audio.add("chime", file);
-
-    let mut last_chime: Option<(u8, u8)> = None;
-
-    let (h, m) = local_time(offset);
-    last_chime = Some((h, m));
-    play_sound(h, "chime", audio);
+    files.iter()
+        .for_each(|f| audio.add(f, f.to_string() + ".wav"));
+    let mut last_chime = (true, 0, 0, 0);
+    loop {
+        let time = get_time((0, 0, 0));
+        let chime = get_bells(time);
+        println!("{chime:?}");
+        if chime.0 != last_chime.0 {play_sound(1, "grande1", &mut audio)}
+        if chime.1 != last_chime.1 {play_sound(chime.1, "hour", &mut audio)};
+        if chime.2 != last_chime.2 {play_sound(chime.2, "minute", &mut audio)};
+        last_chime = chime;
+        thread::sleep(Duration::from_secs(5));
+    }
 }
 
-fn local_time(offset: UtcOffset) -> (u8, u8) {
-    let time = OffsetDateTime::now_utc()
-        .to_offset(offset);
-    (time.hour(), time.minute())
+fn get_time(offset: (u64, u64, u64)) -> (u8, u8, u8) {
+    let now = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Time before Unix epoch is not supported")
+        .as_secs();
+    let h = (now / 3600 + offset.0) % 24;
+    let m = (now / 60   + offset.1) % 60;
+    let s = (now        + offset.2) % 60;
+    (h as u8, m as u8, s as u8)
 }
 
-fn play_sound(n: u8, file: &str, mut audio: Audio) {
+fn get_bells(time: (u8, u8, u8)) -> (bool, u8, u8, u8) {
+    let (pm, h) = match time.0 {
+        0       => (false, 12),
+        1..=11  => (false, time.0),
+        12      => (true,  time.0),
+        13..=23 => (true,  time.0 - 12),
+        _       => (false, 0)
+    };
+    let m = time.1 / 15;
+    (pm, h, m, 0)
+}
+
+fn play_sound(n: u8, file: &str, audio: &mut Audio) {
     for _ in 0..n {
         audio.play(file);
         audio.wait();
--- src/sonnerie.rs
+++
@@ -1,8 +0,0 @@
-pub struct Sonnerie {
-}
-
-impl Sonnerie {
-    fn new() -> Self {
-        Sonnerie{}
-    }
-}
