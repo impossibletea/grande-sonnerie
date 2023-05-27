0033b369672dda9491bc42d8b22f19ee92725ba8
Zakhar Tarshis <impossibletea@icloud.com>
2023-05-27 14:20:50 +0300

    Fix offset, add style

--- src/main.rs
+++ src/main.rs
@@ -6,52 +6,47 @@ use std::{
 use rusty_audio::Audio;
 
 fn main() {
-    let files = [
-        "grande1",
-        "grande2",
-        "hour",
-        "minute"
-    ];
     let mut audio = Audio::new();
-    files.iter()
-        .for_each(|f| audio.add(f, f.to_string() + ".wav"));
+    let files = [ "grande"
+                , "hour"
+                , "minute"
+                ];
+    files
+    .iter()
+    .for_each(|f| audio.add(f, f.to_string() + ".wav"));
     let mut last_chime = (true, 0, 0, 0);
     loop {
         let time = get_time((0, 0, 0));
         let chime = get_bells(time);
-        println!("{chime:?}");
-        if chime.0 != last_chime.0 {play_sound(1, "grande1", &mut audio)}
-        if chime.1 != last_chime.1 {play_sound(chime.1, "hour", &mut audio)};
-        if chime.2 != last_chime.2 {play_sound(chime.2, "minute", &mut audio)};
+        if chime.0 && !last_chime.0 {play_sound(1,       "grande", &mut audio)}
+        if chime.1 != last_chime.1  {play_sound(chime.1, "hour",   &mut audio)}
+        if chime.2 != last_chime.2  {play_sound(chime.2, "minute", &mut audio)}
         last_chime = chime;
-        thread::sleep(Duration::from_secs(5));
+        thread::sleep(Duration::from_secs(1));
     }
 }
 
-fn get_time(offset: (u64, u64, u64)) -> (u8, u8, u8) {
+fn get_time(offset: (i64, i64, i64)) -> (u64, u64, u64) {
     let now = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .expect("Time before Unix epoch is not supported")
-        .as_secs();
-    let h = (now / 3600 + offset.0) % 24;
-    let m = (now / 60   + offset.1) % 60;
-    let s = (now        + offset.2) % 60;
-    (h as u8, m as u8, s as u8)
+    .duration_since(SystemTime::UNIX_EPOCH)
+    .expect("Time before Unix epoch is not supported")
+    .as_secs()
+    .saturating_add_signed(offset.0 * 3600 + offset.1 * 60 + offset.2);
+    let h = (now / 3600) % 24;
+    let m = (now / 60  ) % 60;
+    let s = (now       ) % 60;
+    println!("{h}:{m}:{s}");
+    (h, m, s)
 }
 
-fn get_bells(time: (u8, u8, u8)) -> (bool, u8, u8, u8) {
-    let (pm, h) = match time.0 {
-        0       => (false, 12),
-        1..=11  => (false, time.0),
-        12      => (true,  time.0),
-        13..=23 => (true,  time.0 - 12),
-        _       => (false, 0)
-    };
+fn get_bells(time: (u64, u64, u64)) -> (bool, u64, u64, u64) {
+    let h = time.0 % 12;
+    let pm = h == 0;   
     let m = time.1 / 15;
     (pm, h, m, 0)
 }
 
-fn play_sound(n: u8, file: &str, audio: &mut Audio) {
+fn play_sound(n: u64, file: &str, audio: &mut Audio) {
     for _ in 0..n {
         audio.play(file);
         audio.wait();
