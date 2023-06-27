6ce7f4d33489522bdb5c135ff9d2e18306ee2e36
Zakhar Tarshis <impossibletea@icloud.com>
2023-06-28 00:02:09 +0300

    Separate movement into its own module

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,20 @@ use std::{
     time::{SystemTime, Duration}
 };
 
-use rusty_audio::Audio;
+use cpal::traits::HostTrait;
 use serde::{Serialize, Deserialize};
 
-type Time  = (u8, u8);
-type Chime = (bool, Time);
+mod movement;
 
 fn main() {
     let config = Config::new();
-    let mut movement = Movement::new(&config.movement);
+    let mut movement = movement::Movement::new(&config.movement);
     println!("{movement}");
-    let mut audio = Audio::new();
+
+    let device =
+        cpal::default_host()
+        .default_output_device()
+        .expect("No output device found");
 
     let path =
         confy::get_configuration_file_path("grande-sonnerie", None)
@@ -28,19 +31,17 @@ fn main() {
             path
             .join(&config.sonnerie)
             .join(sound.to_string() + ".wav");
-        audio.add(sound, sound_path);
     });
 
     loop {
         let time = get_time(&config.offset);
         println!("{:0>2}:{:0>2}", time.0, time.1);
-        let chimes = movement.got_chimes(time);
-        movement.sonne(chimes, &mut audio);
+        movement.sonne(time, &device);
         thread::sleep(Duration::from_secs(1));
     }
 }
 
-fn get_time(offset: &(i64, i64, i64)) -> Time {
+fn get_time(offset: &(i64, i64, i64)) -> movement::Time {
     let now =
         SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
@@ -50,13 +51,6 @@ fn get_time(offset: &(i64, i64, i64)) -> Time {
     let h = (now / 3600) % 24;
     let m = (now / 60  ) % 60;
     (h as u8, m as u8)
-}
-
-fn play_sound(n: u8, file: &str, audio: &mut Audio) {
-    (0..n).for_each(|_| {
-        audio.play(file);
-        audio.wait();
-    });
 }
 
 #[derive(Serialize, Deserialize)]
@@ -79,157 +73,6 @@ impl std::default::Default for Config {
             sonnerie: "coucou".to_string(),
             movement: "casio".to_string(),
         }
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct MovementConfig {
-    grand:       Option<Vec<u8>>,
-    hours:       Option<Vec<u8>>,
-    hours_div:   Option<u8>,
-    minutes:     Option<Vec<u8>>,
-    minutes_div: Option<u8>,
-    twelve_hour: bool,
-    multichime:  bool,
-}
-
-impl std::default::Default for MovementConfig {
-    fn default() -> Self {
-        MovementConfig {
-            grand:       None,
-            hours:       None,
-            hours_div:   Some(1),
-            minutes:     None,
-            minutes_div: None,
-            twelve_hour: false,
-            multichime:  false,
-        }
-    }
-}
-
-struct Movement {
-    am_pm:   u8,
-    grand:   Vec<u8>,
-    hours:   Vec<u8>,
-    minutes: Vec<u8>,
-    multi:   Multichime,
-    past:    Chime,
-}
-
-struct Multichime {
-    enable: bool,
-    h_x:    u8,
-    m_x:    u8,
-}
-
-impl Movement {
-    fn new(config_name: &str) -> Self {
-        let config: MovementConfig =
-            confy::load("grande-sonnerie", config_name)
-            .unwrap_or_default();
-
-        let am_pm: u8 = if config.twelve_hour {12} else {24};
-
-        let mut grand: Vec<u8> = Vec::new();
-        if let Some(vec) = config.grand {
-            vec.iter().for_each(|x| grand.push(*x));
-        }
-
-        let mut hours: Vec<u8> = Vec::new();
-        if let Some(vec) = config.hours {
-            vec.iter().for_each(|x| hours.push(*x));
-        }
-        if let Some(div) = &config.hours_div {
-            (0..=24).for_each(|x| hours.push((x / div) * div));
-        }
-        hours.sort();
-        hours.dedup();
-        if hours.contains(&0) {hours.remove(0);}
-
-        let mut minutes: Vec<u8> = Vec::new();
-        if let Some(vec) = config.minutes {
-            vec.iter().for_each(|x| minutes.push(*x));
-        }
-        if let Some(div) = config.minutes_div {
-            (0..=59).for_each(|x| minutes.push((x / div)*div));
-        }
-        minutes.sort();
-        minutes.dedup();
-        if minutes.contains(&0) {minutes.remove(0);}
-
-        let h_x =
-            if let Some(h_div) = config.hours_div {h_div} else {1};
-        let m_x =
-            if let Some(m_div) = config.minutes_div {m_div} else {1};
-        let multi =  Multichime {
-            enable: config.multichime,
-            h_x,
-            m_x,
-        };
-
-        Movement {
-            am_pm,
-            grand,
-            hours,
-            minutes,
-            multi,
-            past:   (false, (0, 0)),
-        }
-    }
-
-    fn got_chimes(&self, time: Time) -> Chime {
-        let h = if time.0 != 0 {time.0} else {24};
-        let m = time.1;
-
-        let g_cs =    self.grand.contains(&h);
-        let h_cs = if self.hours.contains(&h) {h} else {0};
-        let m_cs = if self.minutes.contains(&m) {m} else {0};
-
-        (g_cs, (h_cs, m_cs))
-    }
-
-    fn sonne(&mut self, chimes: Chime, audio: &mut Audio) {
-        if chimes.0 && !self.past.0 {
-            play_sound(1, "grand", audio);
-            self.past.0 = chimes.0;
-        }
-        if chimes.1.0 != self.past.1.0 {
-            let n = if self.multi.enable {
-                chimes.1.0 % self.am_pm / self.multi.h_x
-            } else {1};
-            play_sound(n, "hour",   audio);
-            self.past.1.0 = chimes.1.0;
-        }
-        if chimes.1.1 != self.past.1.1 {
-            let n = if self.multi.enable {
-                chimes.1.1 / self.multi.m_x
-            } else {1};
-            play_sound(n, "minute", audio);
-            self.past.1.1 = chimes.1.1;
-        }
-    }
-}
-
-impl std::fmt::Display for Movement {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let multi = if self.multi.enable {""} else {"not "};
-        write!(f,
-"
-Movement will chime the following:
-    * Grand:
-      {:?}
-    * Hours:
-      {:?}
-    * Minutes:
-      {:?}
-
-Chimes will {}be repeated
-",
-               self.grand,
-               self.hours,
-               self.minutes,
-               multi
-        )
     }
 }
 
